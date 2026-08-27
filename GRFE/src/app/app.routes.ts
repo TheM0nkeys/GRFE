7227f999feff48e542514e87907222d0c0c702c6
pedro <pedro.m.dias.d.moraes@gmail.com>
2026-08-27 20:54:51 -0300

    falta terminar o roteamento

--- GRFE/src/app/app.routes.ts
+++ GRFE/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/layout/login/login.component';
+import { NavbarComponent } from './components/layout/navbar/navbar.component';
+//falta terminar o roteamento
+//e depois de fazer o roteamento, fazer o routeGuard
+export const routes: Routes = [
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
+  {path: 'navbar', component: NavbarComponent/*, canActivate: [roleGuard]*/}
 
-export const routes: Routes = [];
+];
--- GRFE/src/app/components/layout/navbar/navbar.component.ts
+++ GRFE/src/app/components/layout/navbar/navbar.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from "../../../../../node_modules/@angular/router/router_module.d-Bx9ArA6K";
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
