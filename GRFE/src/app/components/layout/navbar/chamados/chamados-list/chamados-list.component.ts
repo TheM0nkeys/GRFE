122288d5bc0bbe9e4aa472c082aaa5d0af6a1402
pedro <pedro.m.dias.d.moraes@gmail.com>
2026-08-27 20:18:15 -0300

    Feat - pastas front-end feitas

---
+++ GRFE/src/app/components/layout/navbar/chamados/chamados-list/chamados-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chamados-list',
+  imports: [],
+  templateUrl: './chamados-list.component.html',
+  styleUrl: './chamados-list.component.scss'
+})
+export class ChamadosListComponent {
+
+}
---
+++ GRFE/src/app/components/layout/navbar/relatorios/relatorios-charts/relatorios-charts.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-relatorios-charts',
+  imports: [],
+  templateUrl: './relatorios-charts.component.html',
+  styleUrl: './relatorios-charts.component.scss'
+})
+export class RelatoriosChartsComponent {
+
+}
