b0a1dfe74d4418476d4b9f8733381517ff1c5c07
rodrigo <rodrigoschmitt2020@gmail.com>
2026-09-17 19:50:03 -0300

    melhoria visual do projeto

--- GRFE/src/app/components/layout/login/login.component.ts
+++ GRFE/src/app/components/layout/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
@@ -18,8 +18,15 @@ import { CommonModule } from '@angular/common';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
   loginForm!: FormGroup;
+  readonly loginBackgrounds = [
+    'assets/login/Pasted Image 3.png',
+    'assets/login/usina-itaipu-1.jpg',
+    'assets/login/usina-itaipu-2.jpg'
+  ];
+  backgroundImage = '';
+  private backgroundTimer?: ReturnType<typeof setInterval>;
 
   constructor(
     private fb: FormBuilder,
@@ -27,11 +34,22 @@ export class LoginComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
+    const initialIndex = Math.floor(Math.random() * this.loginBackgrounds.length);
+    this.backgroundImage = this.loginBackgrounds[initialIndex];
+    this.backgroundTimer = setInterval(() => {
+      const currentIndex = this.loginBackgrounds.indexOf(this.backgroundImage);
+      this.backgroundImage = this.loginBackgrounds[(currentIndex + 1) % this.loginBackgrounds.length];
+    }, 9000);
+
     this.loginForm = this.fb.group({
       username: ['', [Validators.required, Validators.minLength(4)]],
       password: ['', [Validators.required, Validators.minLength(6)]],
       rememberMe: [true]
     });
+  }
+
+  ngOnDestroy(): void {
+    if (this.backgroundTimer) clearInterval(this.backgroundTimer);
   }
 
   onSubmit(): void {
