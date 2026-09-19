2d0926487b375467e4a5755660c3b2223da3c4bd
pedro <pedro.m.dias.d.moraes@gmail.com>
2026-09-18 22:10:32 -0300

    telas terminadas indo atras de fazer o service now

--- GRFE/src/app/components/layout/navbar/navbar.component.ts
+++ GRFE/src/app/components/layout/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
-import { Component, ElementRef, HostListener } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { MdbDropdownModule } from 'mdb-angular-ui-kit/dropdown';
+
 @Component({
   selector: 'app-navbar',
   imports: [RouterOutlet, MdbDropdownModule, RouterLink],
@@ -8,23 +9,23 @@ import { MdbDropdownModule } from 'mdb-angular-ui-kit/dropdown';
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-    isOpen = false;
+    menuAberto = false;
 
-    constructor(private readonly elementRef: ElementRef<HTMLElement>) {}
+    @ViewChild('menuWrapper') menuWrapper!: ElementRef<HTMLElement>;
 
     toggleMenu(): void {
-      this.isOpen = !this.isOpen;
+      this.menuAberto = !this.menuAberto;
     }
 
     @HostListener('document:click', ['$event'])
     fecharAoClicarFora(event: MouseEvent): void {
-      if (this.isOpen && !this.elementRef.nativeElement.contains(event.target as Node)) {
-        this.isOpen = false;
+      if (this.menuAberto && this.menuWrapper && !this.menuWrapper.nativeElement.contains(event.target as Node)) {
+        this.menuAberto = false;
       }
     }
 
     @HostListener('window:scroll')
     fecharAoRolar(): void {
-      this.isOpen = false;
+      this.menuAberto = false;
     }
 }
